122464e5d06b888751db56777dc1452612500a83
alex <alex.szebrag.mcmaster@gmail.com>
2018-11-22 19:43:23 -0500

    add stuff

---
+++ doSomething.java
@@ -0,0 +1,16 @@
+package testing;
+
+public class doSomething {
+	
+	public doSomething()
+	{
+		System.out.println("im over here");
+	}
+	
+	public int number()
+	{
+		
+		return 1;
+	}
+
+}
---
+++ testMain.java
@@ -0,0 +1,12 @@
+package testing;
+
+public class testMain {
+
+	public static void main(String[] args) {
+		System.out.println("dasds");
+		doSomething k = new doSomething();
+		
+
+	}
+
+}
